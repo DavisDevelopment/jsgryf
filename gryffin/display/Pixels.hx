073a2f9f3fdef81389b3302c53e5ffa6e871fef8
Ryan Davis <epikit@gmail.com>
2015-11-09 19:10:41 -0600

    Created gryffin.display.Pixels class

---
+++ gryffin/display/Pixels.hx
@@ -0,0 +1,116 @@
+package gryffin.display;
+
+import js.html.ImageData;
+import js.html.CanvasElement in Can;
+import js.html.Uint8ClampedArray in UArray;
+
+import tannus.graphics.Color;
+import tannus.math.TMath;
+import tannus.geom.Point;
+import tannus.ds.Maybe;
+
+import Std.int;
+
+@:access(gryffin.display.Canvas)
+class Pixels {
+	/* Constructor Function */
+	public function new(owner:Canvas, position:Point, dat:ImageData):Void {
+		canvas = owner;
+		idata = dat;
+		data = idata.data;
+		pos = position;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Get a Color
+	  */
+	public function get(xi:Float, ?y:Float):Color {
+		if (y == null)
+			return getAtIndex(int(xi));
+		else
+			return getAtPos(xi, y);
+	}
+
+	/**
+	  * Get the Color at the given Point
+	  */
+	public inline function getAtPos(x:Float, y:Float):Color {
+		return getAtIndex(index(x, y));
+	}
+
+	/**
+	  * Get the Color at the given index
+	  */
+	public function getAtIndex(i : Int):Color {
+		i *= 4;
+		var col:Color = new Color(data[i], data[i+1], data[i+2], data[i+3]);
+		return col;
+	}
+
+	/**
+	  * Set the Color at the given index
+	  */
+	public function setAtIndex(i:Int, color:Color):Color {
+		i *= 4;
+		data[i] = color.red;
+		data[i+1] = color.green;
+		data[i+2] = color.blue;
+		data[i+3] = (color.alpha!=null?color.alpha:0);
+		return color;
+	}
+
+	/**
+	  * Set the Color at the given Point
+	  */
+	public inline function setAtPos(x:Float, y:Float, color:Color):Color {
+		return setAtIndex(index(x, y), color);
+	}
+
+	/**
+	  * Set the Color at the given Point
+	  */
+	public function set(x:Float, y:Float, color:Color):Color {
+		return setAtPos(x, y, color);
+	}
+
+	/**
+	  * Get index from position
+	  */
+	private inline function index(x:Float, y:Float):Int {
+		return ((int(x) + int(y) * width));
+	}
+
+	/**
+	  * Write [this] PixelData onto the given Canvas
+	  */
+	public function write(target:Canvas, x:Float, y:Float, sx:Float=0, sy:Float=0, ?sw:Maybe<Float>, ?sh:Maybe<Float>):Void {
+		target.context.putImageData(idata, x, y, sx, sy, (sw || width), (sh || height));
+	}
+
+	/**
+	  * Save [this] PixelData to the owner-canvas
+	  */
+	public function save():Void {
+		write(canvas, pos.x, pos.y);
+	}
+
+/* === Computed Instance Fields === */
+
+	public var width(get, never):Int;
+	private inline function get_width() return idata.width;
+
+	public var height(get, never):Int;
+	private inline function get_height() return idata.height;
+
+	public var length(get, never):Int;
+	private inline function get_length() return int(data.length / 4);
+
+/* === Instance Fields === */
+
+	private var idata : ImageData;
+	private var data : UArray;
+	private var canvas : Canvas;
+	private var pos : Point;
+}
