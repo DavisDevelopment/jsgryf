542bb427540f096f0cf5ab244039bd216b5b2c9e
Ryan Davis <epikit@gmail.com>
2015-11-12 00:57:10 -0600

    attempted to refactor gryffin.events.MouseListener to synthesize mouseenter/mouseleave events

--- gryffin/events/MouseListener.hx
+++ gryffin/events/MouseListener.hx
@@ -1,6 +1,6 @@
 package gryffin.events;
 
-import gryffin.core.Stage;
+import gryffin.core.*;
 
 import tannus.events.MouseEvent;
 import tannus.events.EventCreator;
@@ -27,10 +27,11 @@ class MouseListener implements EventCreator {
 	  * Listen for events on the thing
 	  */
 	private function bind():Void {
-		var relevant = ['click', 'mouseup', 'mousedown', 'mousemove'];
+		var relevant = ['click', 'mouseup', 'mousedown'];
 		for (name in relevant) {
 			canvas.addEventListener(name, handle);
 		}
+		canvas.addEventListener('mousemove', handleMove);
 	}
 
 	/**
@@ -72,6 +73,59 @@ class MouseListener implements EventCreator {
 		event.onDefaultPrevented.once(untyped e.preventDefault);
 		event.onPropogationStopped.once(untyped e.stopPropagation);
 		stage.mouseEvent( event );
+	}
+
+	/**
+	  * Handle mouse movement
+	  */
+	private function handleMove():Void {
+		var lastTarget:Null<Entity> = null;
+		function _handle(e : JMEvent):Void {
+			var pos:Point = findPos( e );
+			var mods = findMods( e );
+			var event:MouseEvent = new MouseEvent(e.type, pos, e.button, mods);
+			event.onDefaultPrevented.once(untyped e.preventDefault);
+			event.onPropogationStopped.once(untyped e.stopPropagation);
+
+			var target = getRootTarget( event );
+			switch ([lastTarget, target]) {
+				/* == mouseenter == */
+				case [null, entered]:
+					entered.dispatch('mouseenter', e);
+
+				/* == mouseleave == */
+				case [left, null]:
+					left.dispatch('mouseleave', e);
+
+				/* == mousemove == */
+				case [left, right]:
+					/* == moved within the same entity == */
+					if (left == right) {
+						left.dispatch('mousemove', e);
+					}
+
+					else {
+						left.dispatch('mouseleave', e);
+						right.dispatch('mouseenter', e);
+					}
+			}
+			lastTarget = target;
+			stage.mouseEvent( event );
+		}
+
+		canvas.addEventListener('mousemove', _handle);
+	}
+
+	/**
+	  * get the root event target
+	  */
+	private function getRootTarget(e : MouseEvent):Null<Entity> {
+		for (child in stage.children) {
+			if (child.containsPoint(e.position) && !(child._cached || child.destroyed)) {
+				return child;
+			}
+		}
+		return null;
 	}
 
 /* === Instance Fields === */
