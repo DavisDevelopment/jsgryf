2b1012a55c8093d25d0b8e60ad1643af9962cfd1
Ryan Davis <epikit@gmail.com>
2015-12-25 02:53:05 -0600

    added useful methods to gryffin.display.Canvas

--- gryffin/display/Canvas.hx
+++ gryffin/display/Canvas.hx
@@ -2,6 +2,8 @@ package gryffin.display;
 
 import tannus.ds.Ref;
 import tannus.geom.*;
+import tannus.io.ByteArray;
+import tannus.io.Blob;
 
 import gryffin.display.Ctx;
 import gryffin.display.Context;
@@ -9,6 +11,11 @@ import gryffin.display.Paintable;
 
 import js.html.CanvasElement in NCanvas;
 import js.Browser.document in doc;
+import js.html.Blob in JBlob;
+import js.html.FileReader;
+
+import Math.*;
+import tannus.math.TMath.*;
 
 class Canvas implements Paintable {
 	/* Constructor Function */
@@ -32,6 +39,26 @@ class Canvas implements Paintable {
 	}
 
 	/**
+	  * get a resized copy of [this] Canvas
+	  */
+	public function resized(w:Float, h:Float):Canvas {
+		var copy = Canvas.create(floor(w), floor(h));
+		copy.context.drawImage(canvas, 0, 0, width, height, 0, 0, copy.width, copy.height);
+		return copy;
+	}
+
+	/**
+	  * Scale [this] Canvas
+	  */
+	public function scale(w:Float, ?h:Float):Canvas {
+		if (h == null)
+			h = w;
+		var copy:Canvas = create(floor(w * width), floor(h * height));
+		copy.context.drawImage(canvas, 0, 0, width, height, 0, 0, copy.width, copy.height);
+		return copy;
+	}
+
+	/**
 	  * Paint [this] Canvas onto another
 	  */
 	public function paint(c:Ctx, src:Rectangle, dest:Rectangle):Void {
@@ -43,6 +70,21 @@ class Canvas implements Paintable {
 	  */
 	public function dataURI(?type:String):String {
 		return canvas.toDataURL( type );
+	}
+
+	/**
+	  * Get a Blob from [this] Canvas
+	  */
+	public function toBlob(cb:Blob -> Void, ?type:String) {
+		canvas.toBlob(function(b : JBlob) {
+			var readr = new FileReader();
+			readr.onload = function(e) {
+				var data:ByteArray = ByteArray.ofData(cast e.target.result);
+				var blob = new Blob('blob', (type==null?'image/png':type), data);
+				cb( blob );
+			};
+			readr.readAsArrayBuffer( b );
+		}, type);
 	}
 
 	/**
@@ -74,6 +116,12 @@ class Canvas implements Paintable {
 	public var context(get, never):Ctx;
 	private inline function get_context():Ctx {
 		return _ctx.get();
+	}
+
+	/* the pixels of [this] Canvas */
+	public var pixels(get, never):Pixels;
+	private function get_pixels():Pixels {
+		return context.pixels(0, 0, width, height);
 	}
 
 /* === Instance Methods === */
