d6667ef648797991743aca7d3bb10576d4283f2b
Ryan Davis <epikit@gmail.com>
2016-02-25 03:16:54 -0600

    fixed several major issues in gryffin.display.Context

--- gryffin/display/Context.hx
+++ gryffin/display/Context.hx
@@ -12,8 +12,6 @@ class Context {
 	/* Constructor Function */
 	public function new(ctx : CanvasRenderingContext2D):Void {
 		this.ctx = ctx;
-		states = new Stack();
-		matrix = new Matrix();
 	}
 
 /* === Instance Methods === */
@@ -28,7 +26,7 @@ class Context {
 	/**
 	  * Paint some object onto [this] Context
 	  */
-	public function paint(comp:Paintable, src:Rectangle, dest:Rectangle):Void {
+	public inline function paint(comp:Paintable, src:Rectangle, dest:Rectangle):Void {
 		comp.paint(ctx, src, dest);
 	}
 
@@ -44,92 +42,56 @@ class Context {
 	/**
 	  * Draw a vertex array
 	  */
-	public function drawVertices(vertices : Vertices):Void {
+	public inline function drawVertices(vertices : Vertices):Void {
 		ctx.drawVertices( vertices );
 	}
 
-	/**
-	  * Apply a Matrix to [this]
-	  */
-	public function applyMatrix(m : Matrix):Void {
-		/* if the only non-null transforms are 'tx' and 'ty' */
-		if (m.a == 1 && m.b == 0 && m.c == 0 && m.d == 1) {
-			ctx.translate(m.tx, m.ty);
-		}
-		/* otherwise */
-		else {
-			ctx.setTransform(m.a, m.b, m.c, m.d, m.tx, m.ty);
-		}
-	}
-
-	/**
-	  * Get the current Matrix of [this]
-	  */
-	public function obtainMatrix():Matrix {
-		return matrix.clone();
-	}
-
-	public function save():Void {
+	public inline function save():Void {
 		ctx.save();
-		states.add(State.create( this ));
 	}
 
 
-	public function restore():Void {
+	public inline function restore():Void {
 		ctx.restore();
-		
-		if ( !states.empty ) {
-			states.pop().apply( this );
-		}
 	}
 
 
-	public function scale(x:Float, y:Float):Void {
+	public inline function scale(x:Float, y:Float):Void {
 		ctx.scale(x, y);
-		matrix.scale(x, y);
 	}
 
 
-	public function rotate(angle:Float):Void {
+	public inline function rotate(angle:Float):Void {
 		ctx.rotate( angle );
-		matrix.rotate( angle );
 	}
 
 
-	public function translate(x:Float, y:Float):Void {
+	public inline function translate(x:Float, y:Float):Void {
 		ctx.translate(x, y);
-		matrix.translate(x, y);
 	}
 
 
-	public function transform(a:Float, b:Float, c:Float, d:Float, e:Float, f:Float):Void {
+	public inline function transform(a:Float, b:Float, c:Float, d:Float, e:Float, f:Float):Void {
 		ctx.transform(a, b, c, d, e, f);
 	}
 
 
-	public function setTransform(a:Float, b:Float, c:Float, d:Float, e:Float, f:Float):Void {
+	public inline function setTransform(a:Float, b:Float, c:Float, d:Float, e:Float, f:Float):Void {
 		ctx.setTransform(a, b, c, d, e, f);
-		matrix.a = a;
-		matrix.b = b;
-		matrix.c = c;
-		matrix.d = d;
-		matrix.tx = e;
-		matrix.ty = f;
 	}
 
 
-	public function resetTransform():Void {
+	public inline function resetTransform():Void {
 		ctx.resetTransform();
-		matrix.identity();
 	}
 
 
-	public function createLinearGradient(x0:Float, y0:Float, x1:Float, y1:Float):CanvasGradient {
+	public inline function createLinearGradient(x0:Float, y0:Float, x1:Float, y1:Float):CanvasGradient {
 		return ctx.createLinearGradient(x0, y0, x1, y1);
 	}
 
 
-	public function createRadialGradient(x0:Float, y0:Float, r0:Float, x1:Float, y1:Float, r1:Float):CanvasGradient {
+	public inline function createRadialGradient(x0:Float, y0:Float, r0:Float, x1:Float, y1:Float, r1:Float):CanvasGradient {
 		return ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
 	}
 
@@ -149,42 +111,42 @@ class Context {
 	}
 
 
-	public function beginPath():Void {
+	public inline function beginPath():Void {
 		ctx.beginPath();
 	}
 
 
-	public function fill():Void {
+	public inline function fill():Void {
 		ctx.fill();
 	}
 
 
-	public function stroke():Void {
+	public inline function stroke():Void {
 		ctx.stroke();
 	}
 
 
-	public function drawFocusIfNeeded(element:Element):Void {
+	public inline function drawFocusIfNeeded(element:Element):Void {
 		ctx.drawFocusIfNeeded(element);
 	}
 
 
-	public function drawCustomFocusRing(element:Element):Bool {
+	public inline function drawCustomFocusRing(element:Element):Bool {
 		return ctx.drawCustomFocusRing(element);
 	}
 
 
-	public function clip():Void {
+	public inline function clip():Void {
 		ctx.clip();
 	}
 
 
-	public function isPointInPath(path:Path2D, x:Float, y:Float, winding:CanvasWindingRule):Bool {
+	public inline function isPointInPath(path:Path2D, x:Float, y:Float, winding:CanvasWindingRule):Bool {
 		return ctx.isPointInPath(path, x, y, winding);
 	}
 
 
-	public function isPointInStroke(path:Path2D, x:Float, y:Float):Bool {
+	public inline function isPointInStroke(path:Path2D, x:Float, y:Float):Bool {
 		return ctx.isPointInStroke(path, x, y);
 	}
 
@@ -213,7 +175,7 @@ class Context {
 	/**
 	  * Measure some text when drawn with the current settings
 	  */
-	public function measureText(text:String):TextMetrics {
+	public inline function measureText(text:String):TextMetrics {
 		return ctx.patchedMeasureText( text );
 	}
 
@@ -223,22 +185,22 @@ class Context {
 	}
 
 
-	public function addHitRegion(options:HitRegionOptions):Void {
+	public inline function addHitRegion(options:HitRegionOptions):Void {
 		ctx.addHitRegion(options);
 	}
 
 
-	public function removeHitRegion(id:String):Void {
+	public inline function removeHitRegion(id:String):Void {
 		ctx.removeHitRegion(id);
 	}
 
 
-	public function clearHitRegions():Void {
+	public inline function clearHitRegions():Void {
 		ctx.clearHitRegions();
 	}
 
 
-	public function createImageData(imagedata:ImageData):ImageData {
+	public inline function createImageData(imagedata:ImageData):ImageData {
 		return ctx.createImageData(imagedata);
 	}
 
@@ -256,47 +218,57 @@ class Context {
 		return new Pixels(this, pos, idata);
 	}
 
-	public function putImageData(imagedata:ImageData, dx:Float, dy:Float, dirtyX:Float, dirtyY:Float, dirtyWidth:Float, dirtyHeight:Float):Void {
+	public inline function putImageData(imagedata:ImageData, dx:Float, dy:Float, dirtyX:Float, dirtyY:Float, dirtyWidth:Float, dirtyHeight:Float):Void {
 		ctx.putImageData(imagedata, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight);
 	}
 
 
-	public function setLineDash(segments:Array<Float>):Void {
+	public inline function setLineDash(segments:Array<Float>):Void {
 		ctx.setLineDash(segments);
 	}
 
 
-	public function getLineDash():Array<Float> {
+	public inline function getLineDash():Array<Float> {
 		return ctx.getLineDash();
 	}
 
 
-	public function closePath():Void {
+	public inline function closePath():Void {
 		ctx.closePath();
 	}
 
 
-	public function moveTo(x:Float, y:Float):Void {
+	public inline function moveTo(x:Float, y:Float):Void {
 		ctx.moveTo(x, y);
 	}
 
-
-	public function lineTo(x:Float, y:Float):Void {
-		ctx.lineTo(x, y);
+	/* move to the given Point */
+	public inline function moveToPoint(p : Point):Void {
+		moveTo(p.x, p.y);
 	}
 
 
-	public function quadraticCurveTo(cpx:Float, cpy:Float, x:Float, y:Float):Void {
+	public inline function lineTo(x:Float, y:Float):Void {
+		ctx.lineTo(x, y);
+	}
+
+	/* draw a line to the given Point */
+	public inline function lineToPoint(p : Point):Void {
+		lineTo(p.x, p.y);
+	}
+
+
+	public inline function quadraticCurveTo(cpx:Float, cpy:Float, x:Float, y:Float):Void {
 		ctx.quadraticCurveTo(cpx, cpy, x, y);
 	}
 
 
-	public function bezierCurveTo(cp1x:Float, cp1y:Float, cp2x:Float, cp2y:Float, x:Float, y:Float):Void {
+	public inline function bezierCurveTo(cp1x:Float, cp1y:Float, cp2x:Float, cp2y:Float, x:Float, y:Float):Void {
 		ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
 	}
 
 
-	public function arcTo(x1:Float, y1:Float, x2:Float, y2:Float, radius:Float):Void {
+	public inline function arcTo(x1:Float, y1:Float, x2:Float, y2:Float, radius:Float):Void {
 		ctx.arcTo(x1, y1, x2, y2, radius);
 	}
 
@@ -312,6 +284,14 @@ class Context {
 	  */
 	public inline function drawRect(r : Rectangle):Void {
 		rect(r.x, r.y, r.w, r.h);
+	}
+
+	/**
+	  * draw a line from a Line instance
+	  */
+	public inline function drawLine(line : Line):Void {
+		moveToPoint( line.start );
+		lineToPoint( line.end );
 	}
 
 	/**
@@ -332,7 +312,14 @@ class Context {
 		ctx.quadraticCurveTo(x, y, x + r, y);
 	}
 
-	public function arc(x:Float, y:Float, radius:Float, startAngle:Float, endAngle:Float, ?anticlockwise:Bool):Void {
+	/**
+	  * draw a rounded Rectangle
+	  */
+	public inline function drawRoundRect(rect:Rectangle, radius:Float):Void {
+		roundRect(rect.x, rect.y, rect.w, rect.h, radius);
+	}
+
+	public inline function arc(x:Float, y:Float, radius:Float, startAngle:Float, endAngle:Float, ?anticlockwise:Bool):Void {
 		ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
 	}
 
@@ -431,20 +418,16 @@ class Context {
 	private function get_textBaseline():String return ctx.textBaseline;
 	private function set_textBaseline(v : String):String return (ctx.textBaseline = v);
 
+	/* the width of [this] Context */
 	public var width(get, never):Int;
 	private function get_width() return this.canvas.width;
 
+	/* the height of [this] Context */
 	public var height(get, never):Int;
 	private function get_height() return this.canvas.height;
 
 	/* the underlying CanvasRenderingContext2D object */
 	private var ctx : CanvasRenderingContext2D;
-
-	/* the current Matrix in use by [this] Context */
-	private var matrix : Matrix;
-
-	/* the current StateStack in use by [this] Context */
-	private var states : Stack<State>;
 }
 
 typedef TextMetrics = {
@@ -453,34 +436,3 @@ typedef TextMetrics = {
 	var ascent : Float;
 	var descent : Float;
 };
-
-private class State {
-	/* Constructor Function */
-	public function new():Void {
-		matrix = new Matrix();
-	}
-
-/* === Instance Methods === */
-
-	/**
-	  * Apply [this] State to a Context
-	  */
-	public function apply(c : Context):Void {
-		c.applyMatrix( matrix );
-	}
-
-/* === Instance Fields === */
-
-	public var matrix : Matrix;
-
-/* === Class Methods === */
-
-	/**
-	  * Create a State from a Context
-	  */
-	public static function create(c : Context):State {
-		var s = new State();
-		s.matrix = c.obtainMatrix();
-		return s;
-	}
-}
