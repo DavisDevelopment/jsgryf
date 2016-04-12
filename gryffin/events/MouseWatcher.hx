7fc201cd174c5257641e4d739a31f4f6602b1f87
Ryan Davis <epikit@gmail.com>
2016-04-12 14:32:13 -0500

    attempted to move mouseenter,mouseleave detection to just the single place, for performance. unsuccessful thus far

--- gryffin/events/MouseWatcher.hx
+++ gryffin/events/MouseWatcher.hx
@@ -1,9 +1,12 @@
 package gryffin.events;
 
 import gryffin.core.Stage;
+import gryffin.core.Entity;
 
 import tannus.events.MouseEvent;
 import tannus.geom.Point;
+
+using Lambda;
 
 class MouseWatcher {
 	/* Constructor Function */
@@ -11,6 +14,7 @@ class MouseWatcher {
 		stage = s;
 		lastMousePos = null;
 		lastMove = -1;
+		hovered = new List();
 
 		_listen();
 	}
@@ -45,6 +49,15 @@ class MouseWatcher {
 	private function onmove(e : MouseEvent):Void {
 		lastMousePos = e.position;
 		lastMove = e.date;
+
+		/*
+		var ents = stage.getEntitiesAtPoint( e.position );
+		trace( ents );
+		for (ent in ents) {
+			mouseInEnt(ent, e);
+		}
+		hoveredEntities( e );
+		*/
 	}
 
 	/**
@@ -54,10 +67,60 @@ class MouseWatcher {
 		lastMousePos = null;
 	}
 
+	/**
+	  * handle and the Mouse position falling within the bouds of an Entity
+	  */
+	private function mouseInEnt(entity:Entity, event:MouseEvent):Void {
+		var prev:Bool = entityHovered( entity );
+		var curr:Bool = entity.containsPoint( event.position );
+		entityHovered(entity, curr);
+
+		switch ([prev, curr]) {
+			case [false, true]:
+				var mee = event.clone();
+				mee.type = 'mouseenter';
+
+				entity.dispatch('mouseenter', mee);
+
+			case [true, false]:
+				var mle = event.clone();
+				mle.type = 'mouseleave';
+				entity.dispatch('mouseleave', mle);
+
+			default:
+				null;
+		}
+	}
+
+	private function hoveredEntities(event : MouseEvent):Void {
+		for (entity in hovered) {
+			mouseInEnt(entity, event);
+		}
+	}
+
+	/**
+	  * get/set whether the given Entity is registered as 'hovered'
+	  */
+	private function entityHovered(e:Entity, ?value:Bool):Bool {
+		if (value != null) {
+			if (value && !hovered.has( e )) {
+				hovered.add( e );
+			}
+			else {
+				hovered.remove( e );
+			}
+			return value;
+		}
+		else {
+			return hovered.has( e );
+		}
+	}
+
 /* === Instance Fields === */
 
 	private var lastMousePos : Null<Point>;
 	private var lastMove : Float;
+	private var hovered : List<Entity>;
 
 	public var stage : Stage;
 }
