0dcb786eb42172b3b14ced1f7296dfe148df17db
Ryan Davis <epikit@gmail.com>
2016-08-29 11:32:31 -0500

    finished gryffin.audio.AudioData type

--- gryffin/audio/AudioData.hx
+++ gryffin/audio/AudioData.hx
@@ -18,19 +18,36 @@ using tannus.ds.ArrayTools;
 using tannus.math.TMath;
 
 @:forward
-abstract AudioData<T:Float> (CAudioData<T>) from CAudioData<T> to CAudioData<T> {
+abstract AudioData<T:Float> (IAudioData<T>) from IAudioData<T> to IAudioData<T> {
 	/* Constructor Function */
-	public inline function new(fd : CAudioData<T>):Void {
+	public inline function new(fd : IAudioData<T>):Void {
 		this = fd;
 	}
 
 /* === Instance Methods === */
 
 	@:arrayAccess
-	public inline function get(i : Int):T return this.get( i );
+	public inline function get(i : Int):T return this.get( i );	
+	@:arrayAccess
+	public inline function set(i:Int, v:T):T return this.set(i, v);
+	public inline function clone():AudioData<T> return this.clone();
+	public inline function slice(start:Int, ?end:Int):AudioData<T> return this.slice(start, end);
+	public inline function invert():AudioData<T> return this.invert();
+	public inline function writeTo(other:AudioData<T>, ?offset:Int, ?start:Int, ?end:Int):Void this.writeTo(other, offset, start, end);
+	public inline function copyFrom(other:AudioData<T>, ?offset:Int, ?start:Int, ?end:Int):Void this.copyFrom(other, offset, start, end);
+
+	@:to
+	public inline function toByteArray():ByteArray return this.getByteArray();
 
 /* === Factory Methods === */
 
-	public static inline function byte(a:AudioAnalyser, l:Uint8Array):AudioData<Int> return new ByteAudioData(a, l);
-	public static inline function float(a:AudioAnalyser, l:Float32Array):AudioData<Float> return new FloatAudioData(a, l);
+	@:from
+	public static inline function byte(l : Uint8Array):AudioData<Int> {
+		return new ByteAudioData( l );
+	}
+
+	@:from
+	public static inline function float(l : Float32Array):AudioData<Float> {
+		return new Float32AudioData( l );
+	}
 }
