b202763d7a1a5fa5534f52814b156292c82846db
Ryan Davis <epikit@gmail.com>
2016-08-29 11:37:55 -0500

    Created new gryffin.display.LinkedPixels class

---
+++ gryffin/display/LinkedPixels.hx
@@ -0,0 +1,35 @@
+package gryffin.display;
+
+import js.html.ImageData;
+import js.html.Uint8Array;
+import js.html.CanvasElement in Can;
+import js.html.Uint8ClampedArray in UArray;
+
+import tannus.graphics.Color;
+import tannus.math.TMath;
+import tannus.geom.Point;
+import tannus.geom.Rectangle;
+import tannus.ds.Maybe;
+import tannus.io.ByteArray;
+
+import Std.int;
+
+class LinkedPixels extends CPixels {
+	public function new(owner:Ctx, are:Rectangle, dat:ImageData):Void {
+		super( dat );
+
+		c = owner;
+		area = are;
+	}
+
+/* === Instance Methods === */
+
+	override public function save():Void {
+		c.putImageData(idata, 0, 0, area.x, area.y, area.w, area.h);
+	}
+
+/* === Instance Fields === */
+
+	public var c : Ctx;
+	public var area : Rectangle;
+}
