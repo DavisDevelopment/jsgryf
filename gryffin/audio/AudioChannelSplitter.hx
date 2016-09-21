e9812e3a7d19bb3f8721b5164afdd8332710b04f
Ryan Davis <epikit@gmail.com>
2016-09-20 19:02:30 -0500

    added 'connectChannel' method to gryffin.audio.AudioChannelSplitter

--- gryffin/audio/AudioChannelSplitter.hx
+++ gryffin/audio/AudioChannelSplitter.hx
@@ -1,11 +1,22 @@
 package gryffin.audio;
 
+import js.html.audio.AudioNode in NNode;
+
 import js.html.audio.ChannelSplitterNode in Splitter;
 
 class AudioChannelSplitter extends AudioNode<Splitter> {
 	/* Constructor Function */
 	public function new(c:AudioContext, count:Int):Void {
 		super(c, c.c.createChannelSplitter( count ));
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Connect a Node to [this] on a particular channel
+	  */
+	public inline function connectChannel<T:NNode>(node:AudioNode<T>, channel:Int):Void {
+		connect(node, [channel]);
 	}
 
 /* === Instance Fields === */
