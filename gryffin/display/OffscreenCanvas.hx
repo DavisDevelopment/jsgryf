046656cda3d17b98ad53eeef3041d68219d786f0
DavisDevelopment <epikit@gmail.com>
2017-12-05 23:22:38 -0600

    created new gryffin.display.OffscreenCanvas class

---
+++ gryffin/display/OffscreenCanvas.hx
@@ -0,0 +1,36 @@
+package gryffin.display;
+
+import tannus.ds.Ref;
+import tannus.geom.*;
+import tannus.io.ByteArray;
+import tannus.io.Blob;
+import tannus.html.Blobable;
+import tannus.html.Win;
+
+import gryffin.display.Ctx;
+import gryffin.display.Context;
+import gryffin.display.Paintable;
+
+import js.html.CanvasElement in NCanvas;
+import js.Browser.window;
+import js.Browser.document in doc;
+import js.html.Blob in JBlob;
+import js.html.FileReader;
+
+import Math.*;
+import tannus.math.TMath.*;
+
+using StringTools;
+using tannus.ds.StringUtils;
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.math.TMath;
+using tannus.FunctionTools;
+using tannus.html.JSTools;
+
+class OffscreenCanvas extends Canvas {
+    /* Constructor Function */
+    public function new(width:Int, height:Int):Void {
+        super(untyped Type.createInstance(window.nativeArrayGet('OffscreenCanvas'), [width, height]));
+    }
+}
